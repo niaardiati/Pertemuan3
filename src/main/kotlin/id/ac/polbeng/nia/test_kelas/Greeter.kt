4f285b0185abfb011e47651ea8656375b5c57b5b
Nia Ardiati <ardiatinia01@gmail.com>
2022-03-25 10:39:54 +0700

    Kelas dan Object 1

---
+++ src/main/kotlin/id/ac/polbeng/nia/test_kelas/Greeter.kt
@@ -0,0 +1,6 @@
+package id.ac.polbeng.nia.test_kelas
+class Greeter {
+    fun greet() {
+        println("Hello object world!")
+    } }
+
---
+++ src/main/kotlin/id/ac/polbeng/nia/test_kelas/GreeterTest.kt
@@ -0,0 +1,4 @@
+package id.ac.polbeng.nia.test_kelas
+fun main() {
+    val greeter = Greeter()
+    greeter.greet() }
