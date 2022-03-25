63edc9058b2c0bc43a4f42e8c32346e5855cd86d
Nia Ardiati <ardiatinia01@gmail.com>
2022-03-25 11:15:33 +0700

    Percobaan Initializer Block 4

---
+++ src/main/kotlin/id/ac/polbeng/nia/test_kelas/PersonC.kt
@@ -0,0 +1,9 @@
+package id.ac.polbeng.nia.test_kelas
+//Untuk lebih ringkasnya lagi, deklarasi dan inisialisasi property dapat dilakukan hanya didalam parameter konstruktor utama, seperti contoh kode program berikut
+class PersonC (var firstName: String, var lastName: String, var age: Int) {}
+
+fun main(){
+    val budi = PersonC("Budi", "Gunawan", 21)
+    println("Name : ${budi.firstName} ${budi.lastName}")
+    println("Age : ${budi.age}")
+}
