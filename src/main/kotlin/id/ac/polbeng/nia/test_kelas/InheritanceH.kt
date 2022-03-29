8075bb7aa42f38d0913edaab83ebe45cab99cc66
Nia Ardiati <ardiatinia01@gmail.com>
2022-03-29 21:34:01 +0700

    Percobaan Inheritance (Aturan Overriding)

---
+++ src/main/kotlin/id/ac/polbeng/nia/test_kelas/InheritanceH.kt
@@ -0,0 +1,27 @@
+package id.ac.polbeng.nia.test_kelas
+//Aturan Overriding
+open class Rectangle {
+    open fun draw() {
+        println("From Base Class Rectangle")
+    }
+}
+
+interface Polygon {
+    fun draw() { // interface members are 'open' by default
+        println("From interface Polygon")
+    }
+}
+
+class Square() : Rectangle(), Polygon {
+// The compiler requires draw() to be overridden:
+// override untuk menyediakan implementasinya
+    override fun draw() {
+    super<Rectangle>.draw() // call to Rectangle.draw()
+    super<Polygon>.draw() // call to Polygon.draw()
+}
+}
+
+fun main() {
+    val square = Square()
+    square.draw()
+}
