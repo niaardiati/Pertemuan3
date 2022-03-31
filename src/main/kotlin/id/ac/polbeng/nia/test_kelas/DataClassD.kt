db786ab51283abf8bc78521090eecd94987f8278
Nia Ardiati <ardiatinia01@gmail.com>
2022-03-31 21:17:13 +0700

    Percobaan Data Kelas (Data class toString() method )

---
+++ src/main/kotlin/id/ac/polbeng/nia/test_kelas/DataClassD.kt
@@ -0,0 +1,7 @@
+package id.ac.polbeng.nia.test_kelas
+
+fun main() {
+    val meri = Student("Meri", 20)
+    //Fungsi toString() pada data kelas mengembalikan nilai kembalian String yang merupakan representasi dari objek
+    println(meri.toString())
+}
