32b99a57024f1061af0ba67bd5c41082c62cd828
Nia Ardiati <ardiatinia01@gmail.com>
2022-03-31 20:34:04 +0700

    Percobaan Interfaces (Implementasi Interface)

---
+++ src/main/kotlin/id/ac/polbeng/nia/test_kelas/InterfaceA.kt
@@ -0,0 +1,24 @@
+package id.ac.polbeng.nia.test_kelas
+//interface tidak dapat di instansiasi karena ia tidak memiliki konstruktor. 
+interface MyInterface{
+    var str: String
+    fun demo()
+    fun func(){
+        println("Hello")
+    }
+}
+
+class MyClass: MyInterface{
+    override var str: String = "Rekayasa Perangkat Lunak"
+    override fun demo(){
+        println("Demo Function")
+    }
+
+}
+
+fun main(){
+    val obj = MyClass()
+    obj.demo()
+    obj.func()
+    println(obj.str)
+}
