7f33f44c7e88d2aff89c201e4160dc2e0fa1c3ca
Nia Ardiati <ardiatinia01@gmail.com>
2022-03-31 21:03:46 +0700

    Percobaan Inner Class

---
+++ src/main/kotlin/id/ac/polbeng/nia/test_kelas/NestedInnerClassA.kt
@@ -0,0 +1,15 @@
+package id.ac.polbeng.nia.test_kelas
+//Inner class adalah nested class yang memiliki kata kunci inner dalam pendeklarasiannya. Inner class berbeda dengan nested class, sebuah inner class dapat mengakses data member dari kelas luar
+class OuterA{
+    val a = "Outside Nested Class"
+    inner class Inner {
+        fun callMe() = a
+    }
+}
+
+fun main(){
+    val outer = OuterA()
+    println("Using outer object: ${outer.Inner().callMe()}")
+    val inner = OuterA().Inner()
+    println("Using inner object: ${inner.callMe()}")
+}
